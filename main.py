f0572fa2297fd6fe8bd11ea97ac9bbe66518a4f9
SciencyBagel <87948191+SciencyBagel@users.noreply.github.com>
2023-02-15 19:20:38 -0600

    Working nav bar and refactoring

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import flask
 
 app = flask.Flask(__name__)
 
+# Routes
 @app.route('/')
-def index():
-    return flask.render_template('index.html')
+def home():
+    return flask.render_template('index.html.j2')
 
+@app.route('/about')
+def about():
+    return flask.render_template('about.html.j2')
+
+@app.route('/contact')
+def contact():
+    return flask.render_template('contact.html.j2')
+
+# driver
 if __name__ == '__main__':
     app.run(debug=True)
