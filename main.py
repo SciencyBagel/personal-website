4a63cca3444ad75830021fcc0a95656390696685
SciencyBagel <87948191+SciencyBagel@users.noreply.github.com>
2023-02-15 21:13:13 -0600

    Initial phase complete

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
+posts = {}  # global variable to store posts
 
 # Routes
 @app.route('/')
 def home():
-    return flask.render_template('index.html.j2')
+    # get posts from json file
+    global posts
+    json_file = open("data/posts.json", mode='r')
+    posts = json.load(json_file)
+    json_file.close()
+    
+    return flask.render_template('index.html.j2', posts=posts)
 
 @app.route('/about')
 def about():
@@ -14,6 +22,11 @@ def about():
 @app.route('/contact')
 def contact():
     return flask.render_template('contact.html.j2')
+
+@app.route('/post/#<post_id>')
+def post(post_id):
+    
+    return flask.render_template('post.html.j2', post=posts[post_id])
 
 # driver
 if __name__ == '__main__':
